1a276f7a73c321d1e141430c85da0e8dc3417038
MatMasIt <mattia.mascarello@outlook.it>
2021-09-16 23:32:03 +0200

    Addd ink padding

--- all.sh
+++ all.sh
@@ -1,3 +1,5 @@
+echo "###############################"
+echo "###############################"
 echo "                    "$(date)
 bash meteo.sh
 php feed.php | fold -w 40 | sed 's/^/                /'
